a2c1f3c4fb9670fde0625c829b0a72ab3fa25ca2
CISprague <spragc@rpi.edu>
2016-04-13 14:10:23 -0400

    Resctructured directories

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = 'Spacecraft_Testbed',
+      version = '0.1',
+      description = 'A spacecraft testbed.',
+      url = 'https://github.com/CISprague/Spacecraft_Testbed.git',
+      author = 'Christopher Iliffe Sprague',
+      author_email = 'spragc@rpi.edu',
+      license = 'MIT',
+      packages = ['Spacecraft_Testbed'],
+      zip_sage = False)
