3127c571903c0ea11b2b2fb0634ab90313c5ff07
CISprague <spragc@rpi.edu>
2016-04-14 10:17:42 -0400

    New example

--- Spacecraft_Testbed/Bodies/Massive_Body.py
+++ Spacecraft_Testbed/Bodies/Massive_Body.py
@@ -4,29 +4,10 @@ Massive_Body.py
 This package aims to construct the architecture from which one
 may effectivly define a massive celestial body that is described
 with particular physical attributes.
-<<<<<<< HEAD
-=======
-Copyright (C) Christopher Iliffe Sprague
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
->>>>>>> 4849294fc9af7ef0087f692b4cddebe53ac92762
 '''
 
 #Import numpy linear algebra library
 import numpy as np
-<<<<<<< HEAD
 #Import sys
 import sys
 #Import os
@@ -50,133 +31,18 @@ class Massive_Body(object):
   def __init__(self, name):
     #Catalogue of body imformation
     Body_Information = Make_Massive_Body_Information()
-=======
-import os
-#Import JPL SPICE Kernel with jplephem 2.5
-from jplephem.spk import SPK
-
-def isfloat(value):
-  try:
-    float(value)
-    #Return True if value is indeed a float
-    return True
-  except ValueError:
-    #Rightfully return False if value is not a float
-    return False
-def Make_Body_Information():
-  #Master dictionary for body information
-  Body_Information = {}
-  #Indicies for use in jplephem 2.5
-  jplephem_Indicies = {'Mercury': (1, 199),  #Mercury barycentre with respect to solar system barycentre
-                       'Venus': (2, 299),     #Venus barycentre with respect to solar system barycentre
-                       'Earth': (3, 399),     #Earth barycentre with respect to solar system barycentre
-                       'Mars': (0, 4),        #Mars barycentre with respect to solar system barycentre
-                       'Jupiter': (0, 5),     #Jupiter barycentre with respect to solar system barycentre
-                       'Saturn': (0, 6),      #Saturn barycentre with repsect to solar system barycentre
-                       'Uranus': (0, 7),      #Uranus barycentre with respect to solar system barycentre
-                       'Neptune': (0, 8),     #Neptune barycentre with respect to solar system barycentre
-                       'Pluto': (0, 9),       #Pluto barycentre with respect to solar system barycentre
-                       'Sun': (0, 10),        #Sun centre with respect to solar system barycentre
-                       'Moon': (3, 301)}      #Moon centre with respect to Earth barycentre
-  #Define folder path to planetary information file
-  path = os.getcwd()
-  path = path.split('Spacecraft_Testbed')[0]
-  path = path + 'Spacecraft_Testbed/Bodies/Planetary_Information'
-  #Open text file of planetary information (thanks NASA)
-  Planetary_Information = open(path)
-  #Generate a list of the rows
-  Planetary_Information= Planetary_Information.readlines()
-  #Planetary information list
-  Planetary_Information_List = []
-  Headers = []
-  Siders = []
-  #For each row in file
-  for row in range(len(Planetary_Information)):
-    Row = []
-    #Split the items in the row by the delimeter
-    for item in Planetary_Information[row].split('\t'):
-      #Before the units
-      item = item.split(' (')[0]
-      #Remove the white space and other delimeters from each string item
-      item = item.strip()
-      #Make strings title case in order to validly reference the jplephem_Indices table
-      item = item.title()
-      item = item.replace(',', '')
-      item = item.replace('*', '')
-      item = item.replace(' ', '_')
-      item = item.replace('?', '')
-      if isfloat(item):
-        item = float(item)
-      elif item.lower() == 'yes':
-        item = True
-      elif item.lower() == 'no':
-        item = False
-      #Append the item to the row list
-      Row.append(item)
-    #Append all rows except header row to the list
-    if row != 0 and row != 21:
-      Planetary_Information_List.append(Row[1:])  #Without sider
-      #Append to siders list without units
-      Siders.append(Row[0].split(' (')[0])
-    elif row == 0:
-      Headers = Row[1:]
-  Planetary_Information = Planetary_Information_List
-  for itemi in range(len(Headers)):
-    h = Headers[itemi]
-    Body_Information[h] = {}
-    Body_Information[h]['jplephem_index'] = jplephem_Indicies[h]
-  for rowi in range(len(Siders)):
-    for itemi in range(len(Headers)):
-      #Title of header
-      h = Headers[itemi]
-      #Title of sider
-      s = Siders[rowi]
-      #Multiplier based on sider units
-      if h in ['Mass']:
-        #Convert 10^24 kg to kg
-        m = 1e24
-      elif h in ['Diameter','Escape Velocity','Orbital Velocity']:
-        #Convert km to m
-        m = 1e3
-      elif h in ['Rotation Period', 'Length of Day']:
-        #Convert hours to seconds
-        m = 36e2
-      elif h in ['Distance from Sun', 'Perihelion', 'Aphelion']:
-        #Convert 10^6 km to m
-        m = 1e9
-      elif h in ['Orbital Period']:
-        #Convert days to seconds
-        m = 864e2
-      #If already in SI units or no necessary
-      else:
-        m = 1.0
-      item = Planetary_Information[rowi][itemi]
-      if isfloat(item):
-        Body_Information[h][s] = m * Planetary_Information[rowi][itemi]
-      else:
-        Body_Information[h][s] = item
-  return Body_Information
-class Massive_Body(object):
-  def __init__(self, name):
-    #Catalogue of body imformation
-    Body_Information = Make_Body_Information()
->>>>>>> 4849294fc9af7ef0087f692b4cddebe53ac92762
     #List of body names
     body_names = Body_Information.keys()
     if name not in body_names:
       raise ValueError("Name must be in available ephemerides: ", ','.join(body_names))
     #Name name of body
     self.name = name
-<<<<<<< HEAD
     #Assign the orbital bodies of the massive body
     Assign_Orbital_Bodies(self)
     #History list Julian dates at which ephemerides were referenced at, 
     #the first item being the date at which the simulation comences. 
     #Subsequent dates will be determined by the time step used by 
     #the integration scheme, which may be adaptive in nature.
-=======
-    #History list Julian dates at which ephemerides were referenced at, the first item being the date at which the simulation comences. Subsequent dates will be determined by the time step used by the integration scheme, which may be adaptive in nature.
->>>>>>> 4849294fc9af7ef0087f692b4cddebe53ac92762
     self.times = np.empty(shape = (0, 0), dtype = float)
     #History list of 3 dimensional position vectors [m]
     self.positions = np.empty(shape = (0, 3), dtype = float)
@@ -186,7 +52,6 @@ class Massive_Body(object):
       setattr(Massive_Body, char.lower(), Body_Information[name][char])
   def Position_and_Velocity(self, time):
     '''Returns a list of 2 vectors of barycentric position and velocity'''
-<<<<<<< HEAD
     path = Directory + '/Information/Ephemerides/'
     kernel = path + 'de430.bsp'
     #Does the user have an ephemeris installed
@@ -194,20 +59,12 @@ class Massive_Body(object):
     #If you do not have the ephemeris
     #Assign kernel to a particular ephemerides catalog
     kernel = SPK.open(kernel)    
-=======
-    #Assign kernel to a particular ephemerides catalog
-    kernel = SPK.open('/home/christopher/Documents/Spacecraft_Testbed/Bodies/Ephemerides/de430.bsp')    
->>>>>>> 4849294fc9af7ef0087f692b4cddebe53ac92762
     #Name of the body for which to compute
     Body_Name = self.name
     #Time at which to compute
     Time = time
     #Body imformation table
-<<<<<<< HEAD
     Body_Information = Make_Massive_Body_Information()
-=======
-    Body_Information = Make_Body_Information()
->>>>>>> 4849294fc9af7ef0087f692b4cddebe53ac92762
     #Index for which to compute
     jplephem_Index = Body_Information[Body_Name]['jplephem_index']
     #First inicie of index
@@ -224,11 +81,7 @@ class Massive_Body(object):
       P += p  #True barycentric position
       V += v  #True barycentric velocity
     #Convert kilometres to metres
-<<<<<<< HEAD
     P = P * 1e3
-=======
-    P = P * 1000.
->>>>>>> 4849294fc9af7ef0087f692b4cddebe53ac92762
     #Convert kilometres per day to metres per second
     V = V * 0.0115741
     #Returns numpy arrays, each of size (0,3)
@@ -255,13 +108,3 @@ class Massive_Body(object):
     #If it is desired to print for the sake of debugging
     return p, v
 
-<<<<<<< HEAD
-=======
-Citations = '''
-DE430 JPL Development Ephemeris
-Folkner, W. M., Williams, J. G., Boggs, D. H., Park, R. S., & Kuchynka, P. (2014). The Planetary and Lunar Ephemerides DE430 and DE431.
-
-jplephem 2.5 Python
-Rhodes, B. (2015). jplephem 2.5. Python Software Foundation.
-'''
->>>>>>> 4849294fc9af7ef0087f692b4cddebe53ac92762
--- Spacecraft_Testbed/Bodies/Orbital_Body.py
+++ Spacecraft_Testbed/Bodies/Orbital_Body.py
@@ -4,36 +4,10 @@ Orbital_Body.py
 This package aims to attriute orbital bodies, defined with
 particular physical attirbutes, to previously defined massive
 celestial bodies.
-<<<<<<< HEAD
 '''
 
 #Import Massive_Body.py in order to configure objects
 import Massive_Body
-=======
-Copyright (C) Christopher Iliffe Sprague
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-'''
-
-#Import necessary modules
-import sys
-#Append the directory in which Massive_Body.py is located in
-sys.path.append('../')
-#Import Massive_Body.py in order to configure objects
-from Massive_Body import Massive_Body
->>>>>>> 4849294fc9af7ef0087f692b4cddebe53ac92762
 #Obviously import the numpy linear algebra library
 import numpy as np
 #Import recent wgs84 gravity model for SGP4 propogator
@@ -45,109 +19,7 @@ from sgp4.ext import invjday
 #Import os for file path navigation
 import os
 
-<<<<<<< HEAD
 
-=======
-def TLE_Dictionary(file_path):
-  '''Generates a dictionary from a two line element file'''
-  #Make sure it is a TLE file
-  if not file_path.endswith('.tle'):
-    raise ValueError('The supplied file must be a two line element (.tle).')
-  #Initialise the dictionary
-  collection = {}
-  #Open the two line element file of specified file path
-  TLE = open(file_path)
-  #Initialise the counter to accomodate three line repetition
-  counter = 0
-  #For each line in the TLE file
-  for line in TLE:
-    #Clean the line up
-    line = line.rstrip('\n')
-    line = line.rstrip('\r')
-    #If the line is the header line
-    if counter == 0:
-      #Than this is the orbital body name
-      header = line.strip()
-      #Use underscore for versatility
-      header = header.replace(' ', '_')
-      header = header.replace('-', '_')
-      #If the header is already used in dictionary
-      if header in collection.keys():
-        #Than subscripts must be added
-        #Add count to times header has been used
-        header_count += 1
-        #Add header count subscript to header name
-        header = header + '_' + str(header_count)
-        #Instantiate header's dictionary in collection
-        collection[header] = {}
-      #If the header has not been used yet
-      else:
-        collection[header] = {}
-        #Set the header counter back to zero
-        header_count = 0
-      counter += 1
-    elif counter == 1:
-      collection[header]['line1'] = line
-      counter += 1
-    elif counter == 2:
-      collection[header]['line2'] = line
-      #Reset the counter
-      counter -= 2
-  return collection
-def Orbital_Body_Dictionary(attracting_body):
-  '''This function takes as its arguement an attracting body of the
-  Massive_Body class and generates a dictionary of all its orbital
-  body collections and their individual orbital bodies'''
-  #Instantiate the name of the attracting body
-  attracting_body = attracting_body.name   
-  #Initialise the dictionary of collections and bodies
-  dictionary = {}  
-  #The directory at runtime
-  path = os.getcwd()
-  #Acknowledge the personal absolute path
-  path = path.split('Spacecraft_Testbed')[0]
-  #Concatenate the absolute path with the ubiquitous relative path
-  path = path + 'Spacecraft_Testbed/Bodies/Orbital_Bodies/Attracting_Bodies'
-  path1 = path + '/' + attracting_body 
-  for collection in os.listdir(path1):
-    dictionary[collection] = {}
-    path2 = path1 + '/' + collection
-    for f_name in os.listdir(path2):
-      if f_name.endswith('.tle'):
-        f_path = path2 + '/' + f_name
-        TLE = TLE_Dictionary(f_path)
-        TLE_keys = TLE.keys()
-        for key in TLE_keys:
-          dictionary[collection][key] = TLE[key]
-  return dictionary
-def Assign_Orbital_Bodies(attracting_body):
-  '''This function is to be implemented within the Massive_Body class
-   to assign orbital bodies to a massive body.'''
-  #Name of massive body for which the orbital body will be assigned to
-  name = attracting_body.name
-  #The dictionary of all collections and their orbital bodies
-  catalogue = Orbital_Body_Dictionary(attracting_body)
-  #List of collections
-  collections = catalogue.keys()
-  #For every collection in the catalogue
-  for col in collections:
-    #Create a orbital body collection
-    Collection = Orbital_Body_Collection(attracting_body, col)
-    #Assign the collection to the attracting body
-    setattr(attracting_body, col, Collection)
-    #List of orbital bodies
-    bodies = catalogue[col].keys()
-    #For every orbital body in the collection
-    for body in bodies:
-      #Classify the orbital body under the Orbital_Body class
-      Body = Orbital_Body(attracting_body, Collection, body)
-      #Instantiate the 1st line of the body's TLE
-      Body.TLE_line1 = catalogue[col][body]['line1']
-      #Instantiate the 2nd line of the body's TLE
-      Body.TLE_line2 = catalogue[col][body]['line2']
-      #Assign the body to the attracting body and collection
-      setattr(attracting_body.__dict__[col], body, Body)
->>>>>>> 4849294fc9af7ef0087f692b4cddebe53ac92762
 class Orbital_Body(object):
   '''This class describes an object orbiting about its attracting body.
   Naturally, the initialisation of such an orbital body takes as its
@@ -155,11 +27,7 @@ class Orbital_Body(object):
   def __init__(self, attracting_body, orbital_body_collection, name):
     #If the object used to initialise the orbital body is not derrived from
     #the Massive_Body class, than raise an error.
-<<<<<<< HEAD
     if not isinstance(attracting_body, Massive_Body.Massive_Body):
-=======
-    if not isinstance(attracting_body, Massive_Body):
->>>>>>> 4849294fc9af7ef0087f692b4cddebe53ac92762
       raise ValueError('Cannot assign orbital body to type not of Massive_Body')
     #The name of the orbital body
     self.name = name
@@ -219,23 +87,9 @@ class Orbital_Body_Collection(object):
   by the Orbital_Body class'''
   def __init__(self, attracting_body, name):
     #Check if the attracting body is of the Massive_Body class
-<<<<<<< HEAD
     if not isinstance(attracting_body, Massive_Body.Massive_Body):
-=======
-    if not isinstance(attracting_body, Massive_Body):
->>>>>>> 4849294fc9af7ef0087f692b4cddebe53ac92762
       raise ValueError('Cannot assign collection to type not of Massive_Body')
     #The name of the collection
     self.name = name
     #The name of the attracting body
     self.attracting_body = attracting_body
-<<<<<<< HEAD
-=======
-#Examples
-jd = np.array([2457061.5, 2457062.5, 2457063.5, 2457064.5])
-Earth = Massive_Body('Earth')
-Assign_Orbital_Bodies(Earth)
-Sat = Earth.Cosmos_2251.COSMOS_2251_DEB
-Mars = Massive_Body('Mars')
-print Sat.Position_and_Velocity_WRT(Mars, jd[0])
->>>>>>> 4849294fc9af7ef0087f692b4cddebe53ac92762
